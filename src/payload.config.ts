418a62f9cc244cda874c6f07d9a96667097936f4
Muhammad Iqbal <iqbal.adudu@gmail.com>
2025-07-12 14:39:16 +0300

    done

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -42,8 +42,6 @@ export default buildConfig({
     PracticeSessions,
   ],
   editor: lexicalEditor(),
-  csrf: [process.env.NEXT_PUBLIC_PAYLOAD_URL || 'http://localhost:3000'],
-  cors: [process.env.NEXT_PUBLIC_PAYLOAD_URL || 'http://localhost:3000'],
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
